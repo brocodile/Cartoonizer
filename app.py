416fc92811824bcaf24aefefa4f1373b80b17e10
brosing <firuMe.dronte@protonmail.com>
2024-06-03 11:09:36 +0530

    Write Logic

--- app.py
+++ app.py
@@ -11,16 +11,27 @@ with open('./config.yaml', 'r') as fd:
 sys.path.insert(0, './white_box_cartoonizer/')
 
 import cv2
-from flask import Flask, render_template, make_response, flash
+from flask import Flask, render_template, flash, url_for, redirect
 import flask
 from PIL import Image
 import numpy as np
 import skvideo.io
 if opts['colab-mode']:
-    from flask_ngrok import run_with_ngrok #to run the application on colab using ngrok
+    from flask_ngrok import run_with_ngrok  
 
+import scipy.ndimage
+import matplotlib.pyplot as plt
 
 from cartoonize import WB_Cartoonize
+
+
+
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError
+from flask_bcrypt import Bcrypt
 
 if not opts['run_local']:
     if 'GOOGLE_APPLICATION_CREDENTIALS' in os.environ:
@@ -31,17 +42,97 @@ if not opts['run_local']:
     # Algorithmia (GPU inference)
     import Algorithmia
 
+
+
 app = Flask(__name__)
+
+
+app.secret_key = '9a4f2c8d3b7a1e6f45c8a0bf3267d6b1d4e6f3ca89d2b5f8e3a9c8b5f6v8a35d'
 if opts['colab-mode']:
-    run_with_ngrok(app)   #starts ngrok when the app is run
+    run_with_ngrok(app)  # starts ngrok when the app is run
 
 app.config['UPLOAD_FOLDER_VIDEOS'] = 'static/uploaded_videos'
 app.config['CARTOONIZED_FOLDER'] = 'static/cartoonized_images'
 
 app.config['OPTS'] = opts
 
-## Init Cartoonizer and load its weights 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+bcrypt = Bcrypt(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+
+## Init Cartoonizer and load its weights
 wb_cartoonizer = WB_Cartoonize(os.path.abspath("white_box_cartoonizer/saved_models/"), opts['gpu'])
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=False)
+
+
+class RegisterForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+    password = PasswordField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Password"})
+    submit = SubmitField("Register")
+
+    def validate_username(self, username):
+        existing_user_name = User.query.filter_by(username=username.data).first()
+        if existing_user_name:
+            raise ValidationError("That username already exists. Please choose a different one")
+
+class LoginForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+    password = PasswordField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Password"})
+    submit = SubmitField("Login")
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+
+@app.route('/')
+@app.route('/login', methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user)
+                return redirect(url_for('cartoonize'))
+    
+    return render_template("login.html",form=form)
+
+
+
+
+@app.route('/logout', methods=['GET','POST'])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data)
+        new_user = User(username=form.username.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    return render_template("register.html",form=form)
 
 def convert_bytes_to_image(img_bytes):
     """Convert bytes to numpy array
@@ -52,57 +143,60 @@ def convert_bytes_to_image(img_bytes):
     Returns:
         [numpy array]: Image numpy array
     """
-    
+
     pil_image = Image.open(io.BytesIO(img_bytes))
-    if pil_image.mode=="RGBA":
-        image = Image.new("RGB", pil_image.size, (255,255,255))
+    if pil_image.mode == "RGBA":
+        image = Image.new("RGB", pil_image.size, (255, 255, 255))
         image.paste(pil_image, mask=pil_image.split()[3])
     else:
         image = pil_image.convert('RGB')
-    
+
     image = np.array(image)
-    
+
     return image
 
-@app.route('/')
+
+
 @app.route('/cartoonize', methods=["POST", "GET"])
+@login_required
 def cartoonize():
     opts = app.config['OPTS']
     if flask.request.method == 'POST':
         try:
             if flask.request.files.get('image'):
-                img = flask.request.files["image"].read()
-                
-                ## Read Image and convert to PIL (RGB) if RGBA convert appropriately
-                image = convert_bytes_to_image(img)
+                img = flask.request.files["image"].read() # binary format
 
-                img_name = str(uuid.uuid4())
-                
+                ## Read Image and convert to PIL (RGB) if RGBA convert appropriately
+                image = convert_bytes_to_image(img) #  binary to numpy array
+
+                img_name = str(uuid.uuid4()) 
+
                 cartoon_image = wb_cartoonizer.infer(image)
-                
+
                 cartoonized_img_name = os.path.join(app.config['CARTOONIZED_FOLDER'], img_name + ".jpg")
                 cv2.imwrite(cartoonized_img_name, cv2.cvtColor(cartoon_image, cv2.COLOR_RGB2BGR))
-                
+
                 if not opts["run_local"]:
                     # Upload to bucket
-                    output_uri = upload_blob("cartoonized_images", cartoonized_img_name, img_name + ".jpg", content_type='image/jpg')
+                    output_uri = upload_blob("cartoonized_images", cartoonized_img_name, img_name + ".jpg",
+                                             content_type='image/jpg')
 
                     # Delete locally stored cartoonized image
                     os.system("rm " + cartoonized_img_name)
                     cartoonized_img_name = generate_signed_url(output_uri)
-                    
 
                 return render_template("index_cartoonized.html", cartoonized_image=cartoonized_img_name)
 
             if flask.request.files.get('video'):
-                
+
                 filename = str(uuid.uuid4()) + ".mp4"
                 video = flask.request.files["video"]
                 original_video_path = os.path.join(app.config['UPLOAD_FOLDER_VIDEOS'], filename)
                 video.save(original_video_path)
-                
-                modified_video_path = os.path.join(app.config['UPLOAD_FOLDER_VIDEOS'], filename.split(".")[0] + "_modified.mp4")
-                
+
+                modified_video_path = os.path.join(app.config['UPLOAD_FOLDER_VIDEOS'], filename.split(".")[0] +
+                                                   "_modified.mp4")
+
                 ## Fetch Metadata and set frame rate
                 file_metadata = skvideo.io.ffprobe(original_video_path)
                 original_frame_rate = None
@@ -113,37 +207,50 @@ def cartoonize():
                 if opts['original_frame_rate']:
                     output_frame_rate = original_frame_rate
                 else:
-                    output_frame_rate = opts['output_frame_rate']    
+                    output_frame_rate = opts['output_frame_rate']
 
                 output_frame_rate_number = int(output_frame_rate.split('/')[0])
 
-                #change the size if you want higher resolution :
+                # change the size if you want higher resolution :
                 ############################
                 # Recommnded width_resize  #
                 ############################
-                #width_resize = 1920 for 1080p: 1920x1080.
-                #width_resize = 1280 for 720p: 1280x720.
-                #width_resize = 854 for 480p: 854x480.
-                #width_resize = 640 for 360p: 640x360.
-                #width_resize = 426 for 240p: 426x240.
-                width_resize=opts['resize-dim']
+                # width_resize = 1920 for 1080p: 1920x1080.
+                # width_resize = 1280 for 720p: 1280x720.
+                # width_resize = 854 for 480p: 854x480.
+                # width_resize = 640 for 360p: 640x360.
+                # width_resize = 426 for 240p: 426x240.
+                width_resize = opts['resize-dim']
 
                 # Slice, Resize and Convert Video as per settings
                 if opts['trim-video']:
-                    #change the variable value to change the time_limit of video (In Seconds)
+                    # change the variable value to change the time_limit of video (In Seconds)
                     time_limit = opts['trim-video-length']
                     if opts['original_resolution']:
-                        os.system("ffmpeg -hide_banner -loglevel warning -ss 0 -i '{}' -t {} -filter:v scale=-1:-2 -r {} -c:a copy '{}'".format(os.path.abspath(original_video_path), time_limit, output_frame_rate_number, os.path.abspath(modified_video_path)))
+                        os.system("ffmpeg -hide_banner -loglevel warning -ss 0 -i '{}' -t {} -filter:v scale=-1:-2 -r {} -c:a copy '{}'".format(
+                            os.path.abspath(original_video_path), time_limit, output_frame_rate_number,
+                            os.path.abspath(modified_video_path)))
                     else:
-                        os.system("ffmpeg -hide_banner -loglevel warning -ss 0 -i '{}' -t {} -filter:v scale={}:-2 -r {} -c:a copy '{}'".format(os.path.abspath(original_video_path), time_limit, width_resize, output_frame_rate_number, os.path.abspath(modified_video_path)))
+                        os.system(
+                            "ffmpeg -hide_banner -loglevel warning -ss 0 -i '{}' -t {} -filter:v scale={}:-2 -r {} -c:a copy '{}'".format(
+                                os.path.abspath(original_video_path), time_limit, width_resize,
+                                output_frame_rate_number, os.path.abspath(modified_video_path)))
                 else:
                     if opts['original_resolution']:
-                       os.system("ffmpeg -hide_banner -loglevel warning -ss 0 -i '{}' -filter:v scale=-1:-2 -r {} -c:a copy '{}'".format(os.path.abspath(original_video_path), output_frame_rate_number, os.path.abspath(modified_video_path)))
+                        os.system(
+                            "ffmpeg -hide_banner -loglevel warning -ss 0 -i '{}' -filter:v scale=-1:-2 -r {} -c:a copy '{}'".format(
+                                os.path.abspath(original_video_path), output_frame_rate_number,
+                                os.path.abspath(modified_video_path)))
                     else:
-                        os.system("ffmpeg -hide_banner -loglevel warning -ss 0 -i '{}' -filter:v scale={}:-2 -r {} -c:a copy '{}'".format(os.path.abspath(original_video_path), width_resize, output_frame_rate_number, os.path.abspath(modified_video_path)))
-                
-                audio_file_path = os.path.join(app.config['UPLOAD_FOLDER_VIDEOS'], filename.split(".")[0] + "_audio_modified.mp4")
-                os.system("ffmpeg -hide_banner -loglevel warning -i '{}' -map 0:1 -vn -acodec copy -strict -2  '{}'".format(os.path.abspath(modified_video_path), os.path.abspath(audio_file_path)))
+                        os.system(
+                            "ffmpeg -hide_banner -loglevel warning -ss 0 -i '{}' -filter:v scale={}:-2 -r {} -c:a copy '{}'".format(
+                                os.path.abspath(original_video_path), width_resize, output_frame_rate_number,
+                                os.path.abspath(modified_video_path)))
+
+                audio_file_path = os.path.join(app.config['UPLOAD_FOLDER_VIDEOS'], filename.split(".")[0] +
+                                               "_audio_modified.mp4")
+                os.system("ffmpeg -hide_banner -loglevel warning -i '{}' -map 0:1 -vn -acodec copy -strict -2  '{}'".format(
+                    os.path.abspath(modified_video_path), os.path.abspath(audio_file_path)))
 
                 if opts["run_local"]:
                     cartoon_video_path = wb_cartoonizer.process_video(modified_video_path, output_frame_rate)
@@ -152,17 +259,67 @@ def cartoonize():
                     response = api_request(data_uri)
                     # Delete the processed video from Cloud storage
                     delete_blob("processed_videos_cartoonize", filename)
-                    cartoon_video_path = download_video('cartoonized_videos', os.path.basename(response['output_uri']), os.path.join(app.config['UPLOAD_FOLDER_VIDEOS'], filename.split(".")[0] + "_cartoon.mp4"))
-                
+                    cartoon_video_path = download_video('cartoonized_videos', os.path.basename(response['output_uri']),os.path.join(app.config['UPLOAD_FOLDER_VIDEOS'], filename.split(".")[0] + "_cartoon.mp4"))
+
                 ## Add audio to the cartoonized video
-                final_cartoon_video_path = os.path.join(app.config['UPLOAD_FOLDER_VIDEOS'], filename.split(".")[0] + "_cartoon_audio.mp4")
-                os.system("ffmpeg -hide_banner -loglevel warning -i '{}' -i '{}' -codec copy -shortest '{}'".format(os.path.abspath(cartoon_video_path), os.path.abspath(audio_file_path), os.path.abspath(final_cartoon_video_path)))
+                final_cartoon_video_path = os.path.join(app.config['UPLOAD_FOLDER_VIDEOS'], filename.split(".")[0] +
+                                                         "_cartoon_audio.mp4")
+                os.system("ffmpeg -hide_banner -loglevel warning -i '{}' -i '{}' -codec copy -shortest '{}'".format(os.path.abspath(cartoon_video_path), os.path.abspath(audio_file_path),os.path.abspath(final_cartoon_video_path)))
 
                 # Delete the videos from local disk
-                os.system("rm {} {} {} {}".format(original_video_path, modified_video_path, audio_file_path, cartoon_video_path))
+                os.system("rm {} {} {} {}".format(original_video_path, modified_video_path, audio_file_path,cartoon_video_path))
 
                 return render_template("index_cartoonized.html", cartoonized_video=final_cartoon_video_path)
-        
+
+            if flask.request.files.get('sketch_image'):
+                def dodge(front, back):
+                    result = front * 255 / (255 - back) 
+                    result[result > 255] = 255
+                    result[back == 255] = 255
+                    return result.astype('uint8')
+
+                def grayscale(rgb):
+                    return np.dot(rgb[..., :3], [0.299, 0.587, 0.114])
+
+
+                #img_path = r"C:\Users\DrDim\Desktop\cartoon\sketchify-master\sketchify-master\jlw.jpg" # flask input image
+
+                # Read the image
+                #img = imageio.imread(img_path)
+                # Read the sketch image using PIL
+
+                #sketch_img = Image.open(flask.request.files["sketch_image"])
+
+                # Convert the image to a numpy array
+                img_bytes = flask.request.files["sketch_image"].read()
+                rgb_image = convert_bytes_to_image(img_bytes)
+
+                img_name = str(uuid.uuid4())
+
+                # Convert the image to grayscale
+                gray_img = grayscale(rgb_image)
+
+                # Invert the grayscale image
+                inverted_img = 255 - gray_img
+
+                # Apply Gaussian filter
+                blurred_img = scipy.ndimage.filters.gaussian_filter(inverted_img, sigma=10)
+
+                # Dodge operation
+                result_img = dodge(blurred_img, gray_img)
+
+                # Convert the processed image to bytes
+                # result_bytes = convert_bytes_to_image(result_img)
+
+                cartoonized_img_name = os.path.join(app.config['CARTOONIZED_FOLDER'], img_name + ".jpg")
+                # cv2.imwrite(cartoonized_img_name, cv2.cvtColor(cartoon_image, cv2.COLOR_RGB2BGR))
+                cv2.imwrite(cartoonized_img_name, result_img)
+
+                # Pass the image bytes to the template for rendering
+                return render_template("index_cartoonized.html", cartoonized_image=cartoonized_img_name)
+                
+            
+
         except Exception:
             print(traceback.print_exc())
             flash("Our server hiccuped :/ Please upload another file! :)")
@@ -170,9 +327,13 @@ def cartoonize():
     else:
         return render_template("index_cartoonized.html")
 
+
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     # Commemnt the below line to run the Appication on Google Colab using ngrok
     if opts['colab-mode']:
         app.run()
     else:
         app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+
